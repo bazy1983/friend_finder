c980010febb6fc980d1a9db0bd19c20f56315897
Mohammed Hameed <mohammed.hameed@email.com>
2018-04-22 18:45:09 -0500

    built basic layout and basic files

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs")
+
+
+//reading friend json file
+var obj;
+fs.readFile(path.join(__dirname + '/friends.json'), 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data)
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
+  })
+   
+  app.listen(3000)
+  console.log("server is alive")
