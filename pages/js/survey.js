d1b37c9bf39711315d33cbec0fb33932efb25a77
Mohammed Hameed <mohammed.hameed@email.com>
2018-04-23 01:08:59 -0500

    built survey document with validation logic

--- pages/js/survey.js
+++ pages/js/survey.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
+    var goldenStar = '<i class="sel fas fa-star"></i> '
+    $(".sel").on("click", function(e){
+        var goldenStarDiv = $(this.parentNode.nextElementSibling);
+            //goldenStarDiv.empty();
+        var clickValue = this.attributes.data.value;
+        for(var i = 0; i< clickValue; i++){
+            goldenStarDiv.append(goldenStar);
+        }
+        this.classList.add("selected");
+
+        //disable stars div and change data attribute value 
+        $(this.parentNode.parentNode).addClass("disabled").attr("data", clickValue)
+    })
+})
+
+function isValid(){
+    var validInput = true;
+    var results = $(".stars");
+    var resultArr = [];
+    for(var i = 0; i < results.length; i++){
+        if(results[i].attributes.data.value === "0") validInput = false;   
+    }
+    return validInput;
+}
+
+$("button").on("click", function(){
     
+    console.log(isValid())
+
 })
