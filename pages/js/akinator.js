11a5a901e51ce0e0608251e12753f167b80480fe
Mohammed Hameed <mohammed.hameed@email.com>
2018-06-15 18:47:45 -0500

    biult guess number game

--- pages/js/akinator.js
+++ pages/js/akinator.js
@@ -1,25 +1,42 @@
 $(document).ready(function(){
 
-    $("#startAki").on("click", function(){
-
-        $.get("/api/akinator", "start", function(data){
-            console.log(data)
-            $(".akinatorbody").append($("<p>").html(data.question))
-            for (let i = 0; i < data.answers.length; i++){
-                $(".akinatorbody").append($("<button class = 'akiAnswer' data = '"+i+"'>").text(data.answers[i].answer))
-            }
+    $(".button").on("click", function(){
+        desc = "Now click on card(s) that show your number, then click (Results) button down below"
+        $(this).parent().children().slideUp(1000, function(){
+            parentDiv = $(this).parent().empty().append(`<p>${desc}</p>`)
+            $(".cards").css("opacity" , 1);
+            
         })
     })
 
-    $(".akinatorbody").on("click", ".akiAnswer", function(){
+    $(".cards div img").on("click", function(){
+        $("#final").text("")
+        $(this).toggleClass("glow");
+        if($(".cards div img").hasClass("glow")){
+            $(".results").children().removeClass("hidden");
+        } else {
+            $(".results").children().addClass("hidden");
+        }
+
+    })
+
+    $(".results").on("click", function(){
+        var results = 0;
+        var cards = $(".glow");
+        myPromise = new Promise(function(resolve, reject) {
+            
+                cards.each(function(index, item){
+                    results += parseInt($(item).attr("alt"));
+                })
+            resolve(results)
+        });
+
+        myPromise.then(function(data){
+            $("#final").text(`Your number is ${data}`)
+        })
         
-        let akiAnswer = $(this).attr("data")
-        $.post("/api/akinator-answer", {answer : akiAnswer}, function(data){
-            console.log(data)
-            $(".akinatorbody").empty();
-        })
-    })
 
+    })
 
 
 })
